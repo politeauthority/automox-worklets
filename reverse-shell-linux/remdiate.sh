840d36e71bc18bfe3f9dae9ea16445b4b84bcd5b
Alix <fullertonalix0@gmail.com>
2021-04-25 20:23:35 -0600

    Reorganizing

--- reverse-shell-linux/remdiate.sh
+++ reverse-shell-linux/remdiate.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 # Reverse Tunnel - Remediation
+# Creates a reverse tunnel between a device and an SSH server.
+#
 # Steps
-#  - Check for a SSH key pair to use, if one doesn't exist make it.
+#  - Check for an SSH key pair to use, if one doesn't exist make it.
 #  - Download the SSH server public key and install it in the devices authorized_keys
 #  - Try to establish the SSH tunnel from the device to the remote SSH server.
-
 set -eu
 
 REMOTE_SSH_HOST="64.225.88.152"
 REMOTE_SSH_PORT=22
 REMOTE_SSH_USER="root"
-REMOTE_PUBLIC_KEY=https://f001.backblazeb2.com/file/polite-pub/automox-worklets/automox-remote-2.pub
+REMOTE_PUBLIC_KEY=https://f001.backblazeb2.com/file/polite-pub/automox-worklets/automox-remote.pub
 REMOTE_PRIVATE_KEY="/root/data/openssh/keys/automox-remote"
 EP_TUNNEL_PORT=43022
 EP_USER="root"
@@ -26,7 +27,6 @@ else
 fi
 EP_SSH_PUBLIC_KEY="${EP_SSH_KEY}.pub"
 
-
 # Check for an ssh key pair on the endpoint, if there isn't one generate one and echo it.
 if ! test -f ${EP_SSH_KEY}; then
     echo "Generating SSH key pair.\n"
@@ -35,10 +35,9 @@ if ! test -f ${EP_SSH_KEY}; then
     PUBLIC_KEY=$(cat ${EP_SSH_PUBLIC_KEY})
 fi
 
-
 # Fetch the remote public key and add it to the device's authorized_keys file if it does not exist
 # there already.
-wget ${REMOTE_PUBLIC_KEY} --quiet -O remote_public_key.pub
+curl ${REMOTE_PUBLIC_KEY} --silent --output remote_public_key.pub
 if test -f ${EP_SSH_AUTHORIZED_KEYS}; then
     PUBLIC_KEY_VALUE=$(cat remote_public_key.pub)
     if ! cat ${EP_SSH_AUTHORIZED_KEYS} | grep "${PUBLIC_KEY_VALUE}"; then
@@ -60,7 +59,6 @@ ssh \
   ${REMOTE_SSH_USER}@${REMOTE_SSH_HOST} \
   -p ${REMOTE_SSH_PORT} \
   -i ${EP_SSH_KEY}
-
 
 echo "[OKAY]  SSH tunnel to ${REMOTE_SSH_HOST} established"
 echo "ssh -o StrictHostKeyChecking=no ${EP_USER}@localhost -p ${EP_TUNNEL_PORT} -i ${REMOTE_PRIVATE_KEY}"
